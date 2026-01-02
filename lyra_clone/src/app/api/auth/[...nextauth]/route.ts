9c918cc5df1c03dc09156721763dd3e87714976a
Seng Kuang Yap <syap0014@student.monash.edu>
2026-01-03 00:06:19 +1100

    added account call back

--- lyra_clone/src/app/api/auth/[...nextauth]/route.ts
+++ lyra_clone/src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,47 @@
-import { handlers } from "sk/server/auth";
+import { NextAuthOptions } from 'next-auth'
+import NextAuth from 'next-auth/next'
+import GoogleProvider from 'next-auth/providers/google'
+import { prisma } from '@/lib/prisma'
 
-export const { GET, POST } = handlers;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
+
+const authOption: NextAuthOptions = {
+    session: {
+        // when expires just relogin using jwt
+        strategy: 'jwt'
+    },
+    provider: [
+        GoogleProvider({
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET
+        })
+    ],
+
+    // called when they are directed and redirected
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (!profile?.email) {
+                throw new Error('No profile')
+            }
+
+            await prisma.user.upsert({
+                where: {
+                    email: profile.email,
+                },
+                create: {
+                    email: profile.email,
+                    name: profile.name,
+                },
+                update: {
+                    name: profile.name,
+                },
+            })
+            return true
+        }
+    }
+}
+
+//export information
+const handler = NextAuth(authOption)
+export { handler as GET, handler as POST }
