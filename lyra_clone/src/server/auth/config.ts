0ab467c7d3ab00c0d86e8246497bd6fa3fd8b16c
Seng Kuang Yap <syap0014@student.monash.edu>
2026-01-03 00:42:58 +1100

    added google as signin provider

--- lyra_clone/src/server/auth/config.ts
+++ lyra_clone/src/server/auth/config.ts
@@ -34,7 +34,10 @@ declare module "next-auth" {
 export const authConfig = {
   providers: [
     DiscordProvider,
-    GoogleProvider
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    })
     /**
      * ...add more providers here.
      *
