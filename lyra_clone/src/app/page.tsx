9a487fddc52c1226210241326da8b6d0933a9648
Seng Kuang Yap <syap0014@student.monash.edu>
2026-01-03 02:16:07 +1100

    installed clerk for auth

--- lyra_clone/src/app/page.tsx
+++ lyra_clone/src/app/page.tsx
@@ -1,11 +1,20 @@
 import { auth } from "../server/auth";
-
+import { signIn } from "../server/auth";
 
 export default async function Home() {
   const session = await auth()
   return (
-    <main className="">
-      {JSON.stringify(session)}
+    <main>
+      <div className="min-h-screen grid grid-cols-2">
+        <div className="flex items-center justify-center">
+          test
+        </div>
+        <div className="flex items-center justify-center">
+          esrse
+        </div>
+      </div>
+      
     </main>
+
   )
 }
